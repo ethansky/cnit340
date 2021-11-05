55ac342d2c9b2dec4c52113057cccaf7564f9443
ethansky <fragglekids@gmail.com>
2021-11-05 13:52:16 -0400

    add skeleton file provided by prof

---
+++ lab3/dosutil.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#First Last
+#Purpose: This script ....
+#Last Revision Date:  ...
+#Variables:
+#ARG1 = ...
+#ARG2 = ...
+#ARG3 = ...
+
+#read arguments ..
+ARG1=$1  # ... 
+ARG2=$2
+ARG3=$3
+
+#Chooses a specific set of operations 
+case $ARG1 in
+	#prints author info
+	"author")
+		....
+		;;
+	#prints file contents
+	"type")
+		...
+		;;
+esac
+
+#successful escape code
+exit 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
