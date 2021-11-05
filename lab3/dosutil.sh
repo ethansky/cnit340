fbb569929bf0c4e7843e26db34e8e552b1a4a0bc
ethansky <fragglekids@gmail.com>
2021-11-05 14:59:12 -0400

    phase 1 submission

--- lab3/dosutil.sh
+++ lab3/dosutil.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-#First Last
+#Ethan Emmons
 #Purpose: This script ....
-#Last Revision Date:  ...
+#Last Revision Date:  11/5/21
 #Variables:
-#ARG1 = ...
-#ARG2 = ...
-#ARG3 = ...
-
+#ARG1 = Command/mode
+#ARG2 = Original file
+#ARG3 = Destination/target file
+HELPOUTPUT="author  |\toutputs the author of the script\n
+type [file]  |\toutputs the contents of the file\n
+copy [file] [destination]  |\tmakes a copy of a file\n
+ren [file] [new name]  |\trenames a file\n
+move [file] [destination]  |\tmoves a file\n
+del [file]  |\t deletes a file skipping confirmation\n
+help [file]  |\tdisplays this help and exit"
 #read arguments ..
-ARG1=$1  # ... 
+ARG1=$1
 ARG2=$2
 ARG3=$3
 
@@ -16,11 +22,41 @@ ARG3=$3
 case $ARG1 in
 	#prints author info
 	"author")
-		....
+		echo Emmons, Ethan
 		;;
+	
 	#prints file contents
 	"type")
-		...
+		cat $2
+		;;
+	
+	#copys file
+	"copy")
+		cp $2 $3 1> /dev/null 2> /dev/null
+		;;
+	
+	#renames file
+	"ren")
+		mv $2 $3 1> /dev/null 2> /dev/null
+		;;
+
+	"move")
+		mv $2 $3 1> /dev/null 2> /dev/null
+		;;
+
+	#deletes file skipping confirmation
+	"del")
+		rm -f $2 1> /dev/null 2> /dev/null
+		;;
+	
+	#outputs supported commands, action, and required parameters
+	"help")
+		echo -e $HELPOUTPUT
+		;;
+
+	#catch all other unsupported commands
+	*)
+		echo -e $HELPOUTPUT
 		;;
 esac
 
