a6f252db12c591fe32a74e6aaa0b0fab44e6d0a5
ethansky <fragglekids@gmail.com>
2021-12-01 00:53:52 -0500

    lab 4 phase 1 submission

--- lab4/backup.sh
+++ lab4/backup.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 #Ethan Emmons
 #Purpose: This script reads a configuration file and automates backups of directories
-#Last Revision Date: 11/29/21
+#Last Revision Date: 12/1/21
 #Variables: 
+#COMPRESSION: compression method
+#EMAIL: email to send logs to
+#BACKUP_TARGET: where to store backup
+#TARGET_TYPE: filesystem for BACKUP_TARGET
+#TARGET_SERVER: DNS/IP of target server
+#TARGET_FS: mount point of the BACKUP_TARGET
+#USER: username of the SMB share
+#PASSWORD: password of the SMB share
 
+#REGEX to match lines that do not start with a # and contains an =
+eval "$(grep '^[^#]*?*=?*' backup.conf)"
+echo "Compression method: $COMPRESSION"
+echo "Email: $EMAIL"
+echo "Backup target: $BACKUP_TARGET"
+echo "Target type: $TARGET_TYPE"
+echo "Target server: $TARGET_SERVER"
+echo "Target filesystem: $TARGET_FS"
+echo "SMB username: $USER"
+echo "SMB password: $PASSWORD"
 
-
-# grep '^[^#]*?*:?*' backup.conf | cut -d: -f
+HOME_ENTRIES=$(grep '^[^#]*?*:?*' backup.conf | cut -d: -f1)
+echo -n "Home directories: ${HOME_ENTRIES[@]}" | tr '\n' ' '
